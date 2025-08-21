4e50c40a18275e5f05f4b33bcb0963fdb9f54b40
kazheko <kazheko@xaker.ru>
2025-08-21 12:45:46 +0200

    remove redundant files

--- src/ArcXray.Core/RepositoryStructure/Models/PackageInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ArcXray.Core.RepositoryStructure.Models
-{
-    /// <summary>
-    /// Represents a NuGet package with its name and version.
-    /// </summary>
-    public record PackageInfo(string Name, string Version);
-}
--- src/ArcXray.Core/RepositoryStructure/Models/RepositoryInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ArcXray.Core.RepositoryStructure.Models
-{
-    /// <summary>
-    /// Represents a repository with .NET solution.
-    /// </summary>
-    public record RepositoryInfo(List<SolutionInfo> Solutions);
-}
